0c7c90a55a61a0df6d6912afca87bb51c5b87247
Felipe Novaes <felipe.novaes@kroton.com.br>
2021-05-07 15:07:52 -0300

    Add getWallet query

--- graphql/src/resolverMap.ts
+++ graphql/src/resolverMap.ts
@@ -1,9 +1,18 @@
 import { IResolvers } from "graphql-tools";
+const wallet = {
+  id: 1,
+  name: "caixa 2 do matheus",
+  balance: 100.50
+}
+
 const resolverMap: IResolvers = {
   Query: {
     helloWorld(_: void, args: void): string {
       return `Hello world!`;
     },
+    getWallet(_: void, args: void){
+      return wallet
+    }
   },
 };
 export default resolverMap;
